3697397b54093cedc66956396bd3ce13811c3631
yug jain <yugjain7373@gmail.com>
2026-06-05 19:14:03 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprint(w, "ParseForm() error %v", err)
+		return
+	}
+	fmt.Fprint(w, "Post request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprint(w, "Name = %s\n", name)
+	fmt.Fprint(w, "Address = %s\n", address)
+
+}
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found ", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported ", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello!")
+
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
